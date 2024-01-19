4b4abe03a177c317b9f9b6a4a3573a7f419c19f9
ppotatoG <thgus2270@gmail.com>
2024-01-19 23:36:46 +0900

    Feat: 초기 페이지 정리

--- App.tsx
+++ App.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import LoginScreen from './src/screens/LoginScreen.tsx';
-import HomeScreen from './src/screens/HomeScreen.tsx';
-import { createStackNavigator } from '@react-navigation/stack';
-
-const Stack = createStackNavigator<StackParamList>();
-
-type StackParamList = {
-  Login: undefined;
-  Home: undefined;
-};
+import AppNavigator from './src/navigation/AppNavigator.tsx';
 
 const App = (): React.JSX.Element => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={'Home'}>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <AppNavigator />
     </NavigationContainer>
   );
 };
