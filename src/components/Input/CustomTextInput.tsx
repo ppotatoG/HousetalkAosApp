65fa2513b31552d9748e3cff2a45082f31505dc9
ppotatoG <thgus2270@gmail.com>
2024-02-28 21:00:11 +0900

    Add: onFocus, onBlur 상태 추가

--- src/components/Input/CustomTextInput.tsx
+++ src/components/Input/CustomTextInput.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, TextInput } from 'react-native';
+
 interface TextInputProps {
   placeholder: string;
   value: string;
@@ -7,6 +8,7 @@ interface TextInputProps {
   keyboardType: 'default' | 'email-address' | 'numeric' | 'phone-pad';
   secureTextEntry?: boolean;
 }
+
 const CustomTextInput = ({
   placeholder,
   value,
@@ -14,14 +16,18 @@ const CustomTextInput = ({
   keyboardType,
   secureTextEntry,
 }: TextInputProps) => {
+  const [isFocused, setIsFocused] = useState(false);
+
   return (
     <TextInput
-      style={styles.input}
+      style={isFocused ? styles.inputFocused : styles.input}
       placeholder={placeholder}
       value={value}
       onChangeText={onChangeText}
       keyboardType={keyboardType}
       secureTextEntry={secureTextEntry}
+      onFocus={() => setIsFocused(true)}
+      onBlur={() => setIsFocused(false)}
     />
   );
 };
@@ -30,6 +36,15 @@ const styles = StyleSheet.create({
   input: {
     backgroundColor: '#F3F4F5',
     borderRadius: 4,
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+  },
+  inputFocused: {
+    backgroundColor: '#F3F4F5',
+    borderColor: '#000000',
+    borderRadius: 4,
+    borderStyle: 'solid',
+    borderWidth: 2,
     paddingHorizontal: 16,
     paddingVertical: 12,
   },
